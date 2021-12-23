00096731d39e7f0fb6d1d97ad252b5f2e2374e81
Jake Russell <jake.russell2@virginmediao2.co.uk>
2021-12-23 13:28:12 +0000

    Initial Commit

---
+++ Cryptocurrency/CryptocurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyApp.swift
+//  Cryptocurrency
+//
+//  Created by Russell, Jake 01 on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
